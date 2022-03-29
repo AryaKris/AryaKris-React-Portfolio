252c7b83d3c3861afdd4413312c3802206b30f57
Arya Krishna <dr.aryakris@outlook.com>
2022-03-29 00:35:03 -0700

    started code -downloaded react app, bootstrap and workimg on App.js

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# AryaKris-React-Portfolio
+create a portfolio using your new React skills and deploy this application to GitHub Pages.
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+      <main>
+
+        <div className="row">
+
+          <div className="col">
+
+
+            <img src="" alt="biopic"></img>
+          </div>
+
+          <div className="col">
+            <h1>Arya Krishna</h1>
+            <p>Get to know me</p>
+
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
