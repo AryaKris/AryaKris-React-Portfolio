67ab3e0f24eb4797c24f298e03b1f5f0f846da94
Arya Krishna <dr.aryakris@outlook.com>
2022-04-01 23:46:10 -0700

    projects page updates with link to github repo

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import projectItems from "../data/projects.json";
 
-export default function Projects() {
+const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>
-
-        </div>
+        <section className="bg-primary">
+            <div className="container p-3">
+                <div className="row g-3">
+                    {projectItems.map(({ id, title, url, image, description }) => (
+                        <div className="col-lg-6" key={id}>
+                            <div className="card h-100">
+                                <img src={image} className="card-img-top" alt={`A sample image of the ${title} project`} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{title}</h5>
+                                    <p className="card-text">{description}</p>
+                                </div>
+                                <div className="card-footer text-end">
+                                    <a href={url} target="_blank" className="btn btn-primary">View the Github</a>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 }
+export default Projects;
