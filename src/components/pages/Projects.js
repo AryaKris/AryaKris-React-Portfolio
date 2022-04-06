0cf3923ab9999b28ef6c27890da387c63a465ba0
Arya Krishna <dr.aryakris@outlook.com>
2022-04-05 18:39:43 -0700

    contact form and updating images

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import projectItems from "../data/projects.json";
+import dailyPlanner from "../../images/daily-planner.png"
+import projectOne from "../../images/project1.png"
+import weatherDashboard from "../../images/weather-dashboard.png"
 
 const Projects = () => {
+    const imageArray = [projectOne,dailyPlanner,weatherDashboard]
     return (
         <section className="bg-primary">
             <div className="container p-3">
                 <div className="row g-3">
-                    {projectItems.map(({ id, title, url, image, description }) => (
+                    {projectItems.map(({ id, title, url, image, description },i) => (
                         <div className="col-lg-6" key={id}>
                             <div className="card h-100">
-                                <img src={image} className="card-img-top" alt={`A sample image of the ${title} project`} />
+                                <img src={imageArray[i]} className="card-img-top" alt={`A sample image of the ${title} project`} />
                                 <div className="card-body">
                                     <h5 className="card-title">{title}</h5>
                                     <p className="card-text">{description}</p>
